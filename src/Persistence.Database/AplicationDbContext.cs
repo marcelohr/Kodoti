0975904a49051e6f7a970b9af6ba8fdeb8cf7542
Marcelo Hernández <mhernandezruiz007@gmail.com>
2022-10-10 20:00:55 -0600

    Initialize Db Migrations

--- src/Persistence.Database/AplicationDbContext.cs
+++ src/Persistence.Database/AplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Model;
+using Persistence.Database.Config;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,9 +9,19 @@ namespace Persistence.Database
 {
     public class AplicationDbContext : DbContext
     {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options) { }
+
         public DbSet<Product> Products { get; set; }
         public DbSet<Client> Clients { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            new ClientConfig(builder.Entity<Client>());
+            new ProductConfig(builder.Entity<Product>());
+        }
     }
 }
---
+++ src/Persistence.Database/Config/ProductConfig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persistence.Database.Config
+{
+    public class ProductConfig
+    {
+        public ProductConfig(EntityTypeBuilder<Product> entityTypeBuilder)
+        {
+            entityTypeBuilder.Property(x => x.Name).IsRequired().HasMaxLength(100);
+            entityTypeBuilder.Property(x => x.Description).IsRequired().HasMaxLength(250);
+        }
+    }
+}
