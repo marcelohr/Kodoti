5a67b6fbb7bfd5b8884681d187e686d0e0010425
Marcelo Hernández <mhernandezruiz007@gmail.com>
2022-10-13 14:32:15 -0600

    ProductDTO

---
+++ src/Model.DTOs/ProductDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.DTOs
+{
+    public class ProductDto
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class ProductCreateDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class ProductUpdateDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
