26cd201942231b7929007c4dd41b64148e34edaf
Marcelo Hernández <mhernandezruiz007@gmail.com>
2022-10-14 19:27:14 -0600

    Implement Order Controller

---
+++ src/Core.Api/Controllers/OrderController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Model.DTOs;
+using Service;
+using Service.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Api.Controllers
+{
+    [ApiController]
+    [Route("orders")]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+        public OrderController(IOrderService orderService) => this._orderService = orderService;
+
+        [HttpGet]
+        public async Task<ActionResult<DataCollection<OrderDto>>> GetById(int page, int take) => 
+            await _orderService.GetAll(page, take);
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<OrderDto>> GetById(int id) => await _orderService.GetById(id);
+
+        [HttpPost]
+        public async Task<ActionResult> Create(OrderCreateDto model)
+        {
+            var result = await _orderService.Create(model);
+            return CreatedAtAction(
+                "GetById",
+                new { id = result.OrderId },
+                result
+            );
+        }
+    }
+}
