3a06b13de918b969f4dc397456254f61fcf3394c
Marcelo Hernández <mhernandezruiz007@gmail.com>
2022-10-14 19:29:59 -0600

    order model fixes

--- src/Persistence.Database/Migrations/20221015012856_OrderDetail.Designer.cs
+++ src/Persistence.Database/Migrations/20221015012856_OrderDetail.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persistence.Database;
 
 namespace Persistence.Database.Migrations
 {
     [DbContext(typeof(AplicationDbContext))]
-    partial class AplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221015012856_OrderDetail")]
+    partial class OrderDetail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Persistence.Database/Migrations/20221015012856_OrderDetail.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Database.Migrations
+{
+    public partial class OrderDetail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Orders_OrderId1",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_OrderId1",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "OrderId1",
+                table: "Orders");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OrderId1",
+                table: "Orders",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_OrderId1",
+                table: "Orders",
+                column: "OrderId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Orders_OrderId1",
+                table: "Orders",
+                column: "OrderId1",
+                principalTable: "Orders",
+                principalColumn: "OrderId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
