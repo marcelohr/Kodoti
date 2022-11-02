00c30dd615c2d95b177739777377413a9957c620
Marcelo Hernández <mhernandezruiz007@gmail.com>
2022-11-02 17:07:07 -0600

    configuring some project params

---
+++ src/SPA.Client/Controllers/ConfigController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SPA.Client.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConfigController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public ConfigController(IConfiguration configuration) => this._configuration = configuration;
+
+        [HttpGet]
+        public ActionResult Index() => Ok(new { 
+            ApiUrl = _configuration.GetValue<string>("ApiUrl")
+        });
+    }
+}
